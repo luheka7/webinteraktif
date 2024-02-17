4d6809b3492a6ecddba64b9e484b1323117344ca
luheka <luhekaalfridawijayanti7@gmail.com>
2024-02-17 04:00:19 -0800

    r1

---
+++ lat1.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+st.write("Coding Club 2024")
+df = pd.DataFrame({
+    'No': [1,2,3,4],
+    'Nama':['zea','feli','natax','tiel'],
+    'nilai':[88,80,84,76]
+})
+
+df
